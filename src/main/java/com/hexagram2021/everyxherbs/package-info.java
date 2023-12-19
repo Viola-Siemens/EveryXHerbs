a2722f6846567891f46f566c5c22acca43ffdb42
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-12-20 00:09:47 +0800

    init commit

---
+++ src/main/java/com/hexagram2021/everyxherbs/package-info.java
@@ -0,0 +1,10 @@
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+
+package com.hexagram2021.everyxherbs;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
